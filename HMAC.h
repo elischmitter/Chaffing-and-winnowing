ce3a29e505ef9a513a236aeb056b3837cb714233
elischmitter <eli.schmitter@gmail.com>
2020-06-27 23:04:24 -0400

    update

--- HMAC.h
+++ HMAC.h
@@ -1,8 +1,13 @@
 #ifndef HMAC_H
 #define HMAC_H
 
-int checkHMAC(char* message,char* key,char* receveMAC);
+#include <openssl/md5.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-char*  makeHMAC(char* message, char* key);
+int checkHMAC(char* message,char* key,char* recevedMAC);
+
+char*  makeHMAC(char* message, char* key,int blockSize);
 
 #endif
